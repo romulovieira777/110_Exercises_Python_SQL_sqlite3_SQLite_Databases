69c4718db94a8a5d02bb71c914f29560d8c8cbcc
Romulo Vieira <romulo.vieira777@gmail.com>
2022-07-01 09:06:42 -0300

    One-To-Many / Many-To-One

---
+++ Section_05_One_To_Many_Many_To_One/10_Exercise.py
@@ -0,0 +1,10 @@
+"""
+Exercise No. 10
+
+Using the built-in sqlite3 package, SQLite database called 'esmartdata_sqlite3' was prepared, which contains the
+following tables:
+    - 'esmartdata_instructor'
+    - 'esmartdata_course'
+
+Create
+"""
