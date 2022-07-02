e9236229dc51da44314d7069791ef513a800b592
Romulo Vieira <romulo.vieira777@gmail.com>
2022-07-02 12:23:45 -0300

    One-To-Many / Many-To-One

---
+++ Section_05_One_To_Many_Many_To_One/12_Exercise.py
@@ -0,0 +1,121 @@
+"""
+Exercise No. 12
+
+Using the built-in sqlite3 package, SQLite database called 'esmartdata_sqlite3' was prepared, which contains the
+following tables:
+    - esmartdata_instructor
+    - esmartdata_course
+
+Create a query that will join the tables "esmartdata_course" and "esmartdata_instructor"(LEFT JOIN) and retrieve all
+records that contain the string "Exer" in the course name(title column).
+
+Display four columns in the output table:
+    - first_name("esmartdata_instructor" table)
+    - last_name("esmartdata_instructor" table)
+    - title("esmartdata_course" table)
+    - subcategory("esmartdata_course" table)
+
+In response, print the result to the console as shown below.
+
+Expected Result:
+
+"""
+import sqlite3
+
+conn = sqlite3.connect("esmartdata.sqlite3")
+cur = conn.cursor()
+
+cur.executescript('''DROP TABLE IF EXISTS "esmartdata_instructor";
+CREATE TABLE IF NOT EXISTS "esmartdata_instructor" (
+    "id" INTEGER NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+DROP TABLE IF EXISTS "esmartdata_course";
+CREATE TABLE IF NOT EXISTS "esmartdata_course" (
+    "id" INTEGER NOT NULL,
+    "title" TEXT NULL,
+    "subtitle" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "subcategory" TEXT NOT NULL,
+    "language" TEXT NOT NULL,
+    "length" TEXT NOT NULL,
+    "rating" REAL NOT NULL,
+    "referral_link" TEXT NOT NULL,
+    "instructor_id" INTEGER NULL,
+    PRIMARY KEY("id" AUTOINCREMENT),
+    FOREIGN KEY("instructor_id") REFERENCES "esmartdata_instructor"(id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);''')
+
+print("Table created successfully!")
+
+cur.executescript('''INSERT INTO "esmartdata_instructor"
+(
+    "id",
+    "first_name",
+    "last_name",
+    "description"
+) 
+VALUES
+(
+    1,
+    "Pawel",
+    "Krakowiak",
+    "Data Scientist/Python Developer/Securities Broker"
+);
+
+INSERT INTO "esmartdata_instructor"
+(
+    "id",
+    "first_name",
+    "last_name",
+    "description"
+)
+VALUES
+(
+    2,
+    "takeITeasy",
+    "Academy",
+    "Akademia Programowania"
+)
+''')
+
+print('Data entered successfully!')
+
+with open('load_esmartdata_course.sql', 'r', encoding='utf-8') as file:
+    sql = file.read()
+
+cur.executescript(sql)
+
+cur.execute('''DROP INDEX IF EXISTS esmartdata_course_instructor_id_idx;''')
+cur.execute('''CREATE INDEX IF NOT EXISTS esmartdata_course_instructor_id_idx ON esmartdata_course('instructor_id');
+''')
+
+print('Index created successfully!')
+
+conn.commit()
+
+cur.execute('''SELECT
+    instructor.first_name
+  , instructor.last_name
+  , course.title
+  , course.subcategory
+FROM
+    esmartdata_course course
+LEFT JOIN
+    esmartdata_instructor instructor
+ON
+    course.instructor_id = instructor.id
+WHERE
+    course.title LIKE '%Exer%'
+''')
+
+for row in cur.fetchall():
+    print(row)
+
+conn.close()
