575840d52353a81a077f823ba466fbead40ff956
Romulo Vieira <romulo.vieira777@gmail.com>
2022-07-07 11:18:51 -0300

    Many-To-Many + Additional Columns

---
+++ Section_07_Many_To_Many_Additional_Columns/01_Exercise.py
@@ -0,0 +1,24 @@
+"""
+Exercise No. 01
+
+Using the built-in sqlite3 package, create a SQLite database named 'esmartdata_sqlite3'.
+
+Perform all the instructions from the create_database.sql script attached to the exercise on this database.
+
+Due to Polish characters, use the 'utf-8' encoding when opening the script(encoding argument of the open() function).
+"""
+import sqlite3
+
+conn = sqlite3.connect("esmartdata.sqlite3")
+cur = conn.cursor()
+
+with open("Querys\create_database.sql", encoding="utf-8") as file:
+    sql = file.read()
+    # cur.executescript(file.read())
+
+cur.executescript(sql)
+
+print("Table created successfully!")
+
+conn.commit()
+conn.close()
