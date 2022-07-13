d0d861f55ffd76dd3a3d375fdf6d14640e712885
Romulo Vieira <romulo.vieira777@gmail.com>
2022-07-13 12:31:34 -0300

    DML - Data Manipulation Language

--- Section_08_DML_Data_Manipulation_Language/Code_Python/02_Exercise.py
+++ Section_08_DML_Data_Manipulation_Language/Code_Python/02_Exercise.py
@@ -1,5 +1,5 @@
 """
-Exercise No. 03
+Exercise No. 02
 
 Using the built-in sqlite3 package, SQLite database called 'esmartdata_sqlite3' was prepared, which contains the
 following tables:
@@ -10,44 +10,37 @@ following tables:
 
 All operations performed so far on this database can be found in the file create_database.sql.
 
-The table 'esmartdata_instructor' contains two records. Using the appropriate statement, insert the next two records
-using the following list of tuples:
-    records = [
-        (3, 'Mike', 'Json', 'Python Developer'),
-        (4, 'Jonathan', 'Parquet', 'Software Engineer'),
-    ]
+The tble 'esmartdata_instructor' contains two records. Using the appropriate statement, insert another record with the
+following tuple:
+    record = (3, 'Mike', 'Json', 'Python Developer')
 
 You shouldn't assemble your query using Python's string operations because doing so is insecure. It makes your program
 vulnerable to an SQL injection attack. Instead, use DB-API's parameter substitution. Put a placeholder ? wherever you
-want to use a value, and then provide a list of tuples as the second argument to the cursor's executemany() method.
+want to use a value, and then provide a tuple of values as the second argument to the cursor's execute() method.
 
 Commit the changes and create a query which will display the contents of the 'esmartdata_instructor' table to the
 console as show below.
 
-Tip: https://docs.python.org/3/library/sqlite3.html#sqlite3.Cursor.executemany
+Tip: https://docs.python.org/3/library/sqlite3.html
 
 Expected Result:
     (1, 'Paweł', 'Krakowiak', 'Data Scientist/Python Developer/Securities Broker')
     (2, 'takeITeasy', 'Academy', 'Akademia Programowania')
     (3, 'Mike', 'Json', 'Python Developer')
-    (4, 'Jonathan', 'Parquet', 'Software Engineer')
 """
 import sqlite3
 
-conn = sqlite3.connect("esmartdata.sqlite3")
+conn = sqlite3.connect("../esmartdata.sqlite3")
 cur = conn.cursor()
 
-with open("Querys\create_database.sql", "r", encoding="utf8") as file:
+with open("../Query/create_database.sql", "r", encoding="utf8") as file:
     sql = file.read()
 cur.executescript(sql)
 
-records = [
-    (3, 'Mike', 'Json', 'Python Developer'),
-    (4, 'Jonathan', 'Parquet', 'Software Engineer'),
-]
+record = (3, 'Mike', 'Json', 'Python Developer')
 
-cur.executemany('''INSERT INTO "esmartdata_instructor" ("id", "first_name", "last_name", "description")
-VALUES(?, ?, ?, ?);''', records)
+cur.execute('''INSERT INTO "esmartdata_instructor" ("id", "first_name", "last_name", "description") 
+VALUES(?, ?, ?, ?);''', record)
 
 print('Data entered successfully!\n')
 
--- Section_08_DML_Data_Manipulation_Language/Code_Python/03_Exercise.py
+++ Section_08_DML_Data_Manipulation_Language/Code_Python/03_Exercise.py
@@ -1,5 +1,5 @@
 """
-Exercise No. 02
+Exercise No. 03
 
 Using the built-in sqlite3 package, SQLite database called 'esmartdata_sqlite3' was prepared, which contains the
 following tables:
@@ -10,37 +10,44 @@ following tables:
 
 All operations performed so far on this database can be found in the file create_database.sql.
 
-The tble 'esmartdata_instructor' contains two records. Using the appropriate statement, insert another record with the
-following tuple:
-    record = (3, 'Mike', 'Json', 'Python Developer')
+The table 'esmartdata_instructor' contains two records. Using the appropriate statement, insert the next two records
+using the following list of tuples:
+    records = [
+        (3, 'Mike', 'Json', 'Python Developer'),
+        (4, 'Jonathan', 'Parquet', 'Software Engineer'),
+    ]
 
 You shouldn't assemble your query using Python's string operations because doing so is insecure. It makes your program
 vulnerable to an SQL injection attack. Instead, use DB-API's parameter substitution. Put a placeholder ? wherever you
-want to use a value, and then provide a tuple of values as the second argument to the cursor's execute() method.
+want to use a value, and then provide a list of tuples as the second argument to the cursor's executemany() method.
 
 Commit the changes and create a query which will display the contents of the 'esmartdata_instructor' table to the
 console as show below.
 
-Tip: https://docs.python.org/3/library/sqlite3.html
+Tip: https://docs.python.org/3/library/sqlite3.html#sqlite3.Cursor.executemany
 
 Expected Result:
     (1, 'Paweł', 'Krakowiak', 'Data Scientist/Python Developer/Securities Broker')
     (2, 'takeITeasy', 'Academy', 'Akademia Programowania')
     (3, 'Mike', 'Json', 'Python Developer')
+    (4, 'Jonathan', 'Parquet', 'Software Engineer')
 """
 import sqlite3
 
-conn = sqlite3.connect("esmartdata.sqlite3")
+conn = sqlite3.connect("../esmartdata.sqlite3")
 cur = conn.cursor()
 
-with open("Querys\create_database.sql", "r", encoding="utf8") as file:
+with open("../Query/create_database.sql", "r", encoding="utf8") as file:
     sql = file.read()
 cur.executescript(sql)
 
-record = (3, 'Mike', 'Json', 'Python Developer')
+records = [
+    (3, 'Mike', 'Json', 'Python Developer'),
+    (4, 'Jonathan', 'Parquet', 'Software Engineer'),
+]
 
-cur.execute('''INSERT INTO "esmartdata_instructor" ("id", "first_name", "last_name", "description") 
-VALUES(?, ?, ?, ?);''', record)
+cur.executemany('''INSERT INTO "esmartdata_instructor" ("id", "first_name", "last_name", "description")
+VALUES(?, ?, ?, ?);''', records)
 
 print('Data entered successfully!\n')
 
