f8b4400f3576c148bb09856cee5f047a3b877492
Romulo Vieira <romulo.vieira777@gmail.com>
2022-07-16 12:05:33 -0300

    Forum Application

--- Section_14_Forum_Application/Query/Count.sql
+++ Section_14_Forum_Application/Query/Count.sql
@@ -2,3 +2,23 @@ SELECT
     COUNT(*)
 FROM
     app_user;
+
+SELECT
+    COUNT(*)
+FROM
+    app_thread;
+
+SELECT
+    COUNT(*)
+FROM
+    app_comment;
+
+SELECT
+    COUNT(*)
+FROM
+    app_group;
+
+SELECT
+    COUNT(*)
+FROM
+    app_group_user;
